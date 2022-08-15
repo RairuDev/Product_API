6363c0d8bf149053c0727843a763ea665cab1cfd
Junich <junich0911@gmail.com>
2022-08-15 20:12:38 +0900

    annotateのセットアップ

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text             not null
+#  name        :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
+# Indexes
+#
+#  index_products_on_name  (name) UNIQUE
+#
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
