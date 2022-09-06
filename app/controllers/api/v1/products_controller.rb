a69902961a3d91378efe2a0ee1799ce390dbb012
Junich <junich0911@gmail.com>
2022-09-07 00:21:48 +0900

    before_actionを追加

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,8 @@
 module Api
   module V1
     class ProductsController < ApplicationController
+      before_action :set_product, only: [:show, :update, :destroy]
+
       def index
         products = if params[:category_id] && Category.find(params[:category_id])
             Product.joins(:category).select("products.id, products.category_id, categories.name AS category_name, products.name, products.description, products.created_at").where(category_id: params[:category_id])
@@ -12,7 +14,6 @@ module Api
       end
 
       def show
-        product = Product.find(params[:id])
         render json: { status: 200, message: "特定の値を取得しました", data: product }
       end
 
@@ -26,13 +27,11 @@ module Api
       end
 
       def destroy
-        product = Product.find(params[:id])
         product.destroy
         render json: { status: "SUCCESS", message: "DELETE the Product", data: product }
       end
 
       def update
-        product = Product.find(params[:id])
         if product.update(product_params)
           render json: { status: "SUCCESS", message: "Update the Product", data: product }
         else
@@ -44,6 +43,10 @@ module Api
 
       def product_params
         params.require(:product).permit(:name, :description, :category_id)
+      end
+
+      def set_product
+        product = Product.find(params[:id])
       end
     end
   end
